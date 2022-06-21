a7e251f4fc8aa6f40c6935adbca888acbaf631f5
Victor Veloso <vitovls37@gmail.com>
2022-06-21 01:49:15 -0300

    feat: Adicionando componente a pagina inicial do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import ListBooks from './components/ListBooks'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello BeonK</h1>
+      <ListBooks />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
